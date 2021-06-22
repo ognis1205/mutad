d886f9226f8289d0d4c9cf1093d134355c4be242
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2021-06-22 17:41:56 +0900

    Updates root project's gradle settings.
    Why:
    
    * ...
    
    Configures setting file to include its subprojects.
    
    * ...

--- data-collectors/src/main/java/io/github/ognis1205/tweet_visualization/data_collectors/DataCollectionDriver.java
+++ data-collectors/src/main/java/io/github/ognis1205/tweet_visualization/data_collectors/DataCollectionDriver.java
@@ -1,4 +1,7 @@
 package io.github.ognis1205.tweet_visualization.data_collectors;
 
 public class DataCollectionDriver {
+    public static void main(String[] args) {
+        System.out.println("Hello, twitter!");
+    }
 }
