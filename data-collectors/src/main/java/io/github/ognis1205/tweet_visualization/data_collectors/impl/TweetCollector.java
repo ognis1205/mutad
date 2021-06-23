8f96b8868715f23d619c0667670cd930e0c7d342
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2021-06-23 16:16:01 +0900

    Implement TweetCollector
    Why:
    
    * ...
    
    Implemented TweetCollector by inheriting the Collector interface. Only basic functionalities are considered. it might need further refactorings.
    
    * ...

--- data-collectors/src/main/java/io/github/ognis1205/tweet_visualization/data_collectors/impl/TweetCollector.java
+++ data-collectors/src/main/java/io/github/ognis1205/tweet_visualization/data_collectors/impl/TweetCollector.java
@@ -15,19 +15,61 @@
  */
 package io.github.ognis1205.tweet_visualization.data_collectors.impl;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesSampleEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import lombok.extern.slf4j.Slf4j;
 import io.github.ognis1205.tweet_visualization.data_collectors.Collector;
 
 /**
  * @author Shingo OKAWA
  * @version 1.0.0
  */
+@Slf4j
 public class TweetCollector implements Collector<String> {
+    /** Blocking queue for storing tweet data. */
+    private BlockingQueue<String> que;
+
+    /** Twitter Http client. */
+    private Client client;
+
+    /**
+     * Initialize `TweetCollector` instance.
+     * <ul>
+     *     <li>Set up your blocking queues: Be sure to the size set properly based on the expected TPS.</li>
+     *     <li>Declare the host you want to connect to.</li>
+     * </ul>
+     * @param consumerKey consumer key of the OAuth.
+     * @param consumerSecret consumer secret key of the OAuth.
+     * @param token token of the OAuth.
+     * @param tokenSecret secret token of the OAuth.
+     */
+    public TweetCollector(String consumerKey, String consumerSecret, String token, String tokenSecret) {
+        this.que = new LinkedBlockingQueue<String>(1000);
+        ClientBuilder builder = new ClientBuilder()
+                .name("Tweet-Visualization-Hosebird-Client")
+                .hosts(new HttpHosts(Constants.STREAM_HOST))
+                .authentication(new OAuth1(consumerKey, consumerSecret, token, tokenSecret))
+                .endpoint(new StatusesSampleEndpoint())
+                .processor(new StringDelimitedProcessor(this.que));
+        this.client = builder.build();
+    }
+
     /**
      * {@inheritDoc}
      */
     @Override
     public void start() {
-        throw new UnsupportedOperationException();
+        log.info("starting up client to collect tweets from Hosebird...");
+        this.client.connect();
+        log.info("starting up client to collect tweets from Hosebird: done!");
     }
 
     /**
@@ -35,7 +77,13 @@ public class TweetCollector implements Collector<String> {
      */
     @Override
     public String collect() {
-        throw new UnsupportedOperationException();
+        String twt = null;
+        try {
+            twt = this.que.poll(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        }
+        return twt;
     }
 
     /**
@@ -43,6 +91,8 @@ public class TweetCollector implements Collector<String> {
      */
     @Override
     public void stop() {
-        throw new UnsupportedOperationException();
+        log.info("shutting down client from twitter...");
+        this.client.stop();
+        log.info("shutting down client from twitter: done!");
     }
 }
